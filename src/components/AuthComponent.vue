fe6ad1e79369cd8f973316f62fb65b57ff1f0f96
Wei Jie <weijieluvbb@hotmail.com>
2023-10-05 23:55:18 +0800

    add some custom config and default authenticator ui for sign in and sign out

---
+++ src/components/AuthComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Authenticator } from '@aws-amplify/ui-vue'
+</script>
+
+<template>
+  <div>
+
+    <h1>Amplify</h1>
+    <Authenticator>
+      <template v-slot="{user, signOut}">
+        <h3>{{JSON.stringify(user)}}</h3>
+        <button @click="signOut">signOut</button>
+      </template>
+    </Authenticator>
+
+  </div>
+</template>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
+import AmplifyVue from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
 
-createApp(App).mount('#app')
+Amplify.configure(awsconfig)
+const app = createApp(App)
+app.use(AmplifyVue)
+app.mount('#app')
